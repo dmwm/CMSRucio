16e13cb6a448b7467d3e2c0db786e8972a4d188d
nataliaratnikova <natasha@fnal.gov>
2017-12-28 17:29:57 -0600

    In updateRSEs.py: added  --add-rse option with some exception handling.

--- docker/CMSRucioClient/scripts/updateRSEs.py
+++ docker/CMSRucioClient/scripts/updateRSEs.py
@@ -8,7 +8,7 @@ import argparse
 from rucio.client.accountclient import AccountClient
 from rucio.client.rseclient import RSEClient
 from rucio.common.exception import Duplicate, RSEProtocolPriorityError, \
-	RSEProtocolNotSupported, RSENotFound
+	RSEProtocolNotSupported, RSENotFound, InvalidObject
 
 def whoami ( account = 'natasha', auth_type='x509_proxy'):
 	"""Runs whoami command for a given account via client tool, requires a valid proxy
@@ -21,6 +21,15 @@ def list_rses(client):
 	for rse in rse_client.list_rses():
 		print (rse['rse'])
 
+def add_rse(client, name):
+	"""Adds an rse """
+	rse_client.add_rse(name)
+	if args.verbosity:
+		print "Added RSE "+name
+		info=rse_client.get_rse(name)
+		for q,v in info.iteritems():
+			print q+" :  ",v
+
 if __name__ == '__main__':
 	parser=argparse.ArgumentParser(
     	description='''
@@ -30,7 +39,9 @@ if __name__ == '__main__':
 	parser.add_argument('--test-auth', action='store_true', \
 		help='executes AccountClient.whoami (use --account option to change identity')
 	parser.add_argument('--account', default='natasha', help=' use account ')
-	parser.add_argument('-v', '--verbose', action='store_true', help='increase output verbosity')
+	parser.add_argument('-v', '--verbose', action='store_true', \
+		help='increase output verbosity')
+	parser.add_argument('--add-rse', help='add RSE')
 	args = parser.parse_args()
 	if args.verbose:
 		print (args) 
@@ -38,8 +49,14 @@ if __name__ == '__main__':
 		whoami(account=args.account)
 	# create re-usable RSE client connection: 
 	rse_client = RSEClient(account=args.account, auth_type='x509_proxy')
-	
 	print "===== Initial list of RSEs:"
 	list_rses(rse_client)
+	try:
+		if args.add_rse:
+			print "===== Adding RSE "+args.add_rse
+			add_rse(rse_client, args.add_rse)
+	except InvalidObject:
+		print "InvalidObject exception: "
+		print str(InvalidObject)
 	print "===== Current list of RSEs:"
 	list_rses(rse_client)
