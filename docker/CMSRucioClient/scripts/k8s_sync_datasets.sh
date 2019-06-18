e2b1ea71d90e687ba442a2ce01a79d5ef57e62cd
Nick Smith <nick.smith@cern.ch>
2019-06-18 20:46:26 +0200

    Actually pull origin master

--- docker/CMSRucioClient/scripts/k8s_sync_datasets.sh
+++ docker/CMSRucioClient/scripts/k8s_sync_datasets.sh
@@ -12,10 +12,7 @@ chmod 400 /tmp/key.pem
 voms-proxy-init2 --debug -rfc -valid 96:00 -cert /tmp/cert.pem -key /tmp/key.pem -out /tmp/x509up -voms cms -rfc -timeout 5
 
 cd /root/CMSRucio
-git remote add sartiran https://github.com/sartiran/CMSRucio.git
-git remote add ericvaandering https://github.com/ericvaandering/CMSRucio.git
-git fetch --all
-git checkout master
+git pull origin master
 export RUCIO_ACCOUNT=root
 
 echo Using config file in $RUCIO_HOME
