75906d7eebb3ffb8b0d96b74ac69bdeb135c79d7
Nick Smith <nick.smith@cern.ch>
2019-06-18 20:53:30 +0200

    Split off sync script config

--- docker/CMSRucioClient/scripts/k8s_sync_sites.sh
+++ docker/CMSRucioClient/scripts/k8s_sync_sites.sh
@@ -17,8 +17,4 @@ echo Using config file in $RUCIO_HOME
 
 cd docker/CMSRucioClient/scripts/
 
-./cmsrses.py --pnn all --select 'T2_\S+' --exclude '\S+RAL\S*' --exclude '\S+Nebraska\S*' --exclude 'T2_MY_\S+' --exclude '\S+CERN\S+' --type real --type temp --type test --fts https://fts3.cern.ch:8446
-./cmsrses.py --pnn all --select 'T1_\S+' --exclude '.*MSS' --type real --type test --fts https://fts3.cern.ch:8446
-./syncaccounts.py --identity "/DC=ch/DC=cern/OU=Organic Units/OU=Users/CN=cmsrucio/CN=430796/CN=Robot: CMS Rucio Data Transfer" --type x509
-./cmslinks.py --phedex_link --overwrite --disable
-
+source sync_sites.sh
