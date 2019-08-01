24028437f6c8cd249678eca379c4eb816a1bc92d
Eric Vaandering <ericvaandering@gmail.com>
2019-08-01 11:17:28 -0500

    Actually delete the rules

--- src/CleanupCampaign.py
+++ src/CleanupCampaign.py
@@ -13,11 +13,17 @@ import pdb
 client = Client()
 client.whoami()
 
-ACCOUNT='transfer_ops'
-SUBSCRIPTION =  'Placement_NanoAODv4'
-#rucio list-rules --subscription transfer_ops Placement_NanoAODv4
+ACCOUNT = 'transfer_ops'
+SUBSCRIPTION = 'Placement_NanoAODv4'
+# rucio list-rules --subscription transfer_ops Placement_NanoAODv4
 
 rules = client.list_subscription_rules(account=ACCOUNT, name=SUBSCRIPTION)
 
+for rule in rules:
+    rule_id = rule['id']
+    dataset = rule['name']
+    expression = rule['res_expression']
 
-pdb.set_trace()
+    print('Cleanup up rule %s (%s) on %s' % (rule_id, expression, dataset))
+
+    client.delete_replication_rule(rule_id=rule_id)
