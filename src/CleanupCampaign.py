0e95847a4240016b52d4eeff7ebe7d36d62e896d
Eric Vaandering <ericvaandering@gmail.com>
2019-08-01 15:04:43 -0500

    Trying better error handling

--- src/CleanupCampaign.py
+++ src/CleanupCampaign.py
@@ -8,6 +8,7 @@ A bit of code to check on the progress of the million file test
 from __future__ import division, print_function
 
 from rucio.client.client import Client
+from rucio.common.exception import RucioException
 from requests.exceptions import ChunkedEncodingError
 import pdb
 
@@ -16,11 +17,16 @@ client.whoami()
 
 ACCOUNT = 'transfer_ops'
 SUBSCRIPTION = 'Placement_NanoAODv4'
-# rucio list-rules --subscription transfer_ops Placement_NanoAODv4
 
-rule_gen = client.list_subscription_rules(account=ACCOUNT, name=SUBSCRIPTION)
+attempts=0
+while attempts < 4:
+    try:
+        attempts += 1
+        rule_gen = client.list_subscription_rules(account=ACCOUNT, name=SUBSCRIPTION)
+    except NotImplementedError:
+        pass
 
-rules = [rule for rule in rule_gen]  # Getting chunking errors if we wait 
+rules = [rule for rule in rule_gen]  # Getting chunking errors if we wait
 
 for rule in rules:
     rule_id = rule['id']
@@ -28,4 +34,7 @@ for rule in rules:
     expression = rule['rse_expression']
 
     print('Cleanup up rule %s (%s) on %s' % (rule_id, expression, dataset))
-    client.delete_replication_rule(rule_id=rule_id)
+    try:
+        client.delete_replication_rule(rule_id=rule_id)
+    except RucioException:
+        print(' Rucio did not respond. Skipping.')
