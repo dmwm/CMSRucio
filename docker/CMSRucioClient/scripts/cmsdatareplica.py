3b97304a72bd1478bd8dea32c59560b2f92c1f7d
Eric Vaandering <ericvaandering@gmail.com>
2019-08-02 09:52:04 -0500

    Need to use the new hostname. Also add some new timers

--- docker/CMSRucioClient/scripts/cmsdatareplica.py
+++ docker/CMSRucioClient/scripts/cmsdatareplica.py
@@ -30,6 +30,17 @@ DEFAULT_SCOPE = 'cms'
 
 REMOVE_CHUNK_SIZE = 20
 
+import monitor #  rucio.core.monitor
+
+try: # New name
+    monitor.SERVER='statsd-exporter-rucio-statsd-exporter'
+    monitor.CLIENT = statsClient(host=monitor.SERVER, port=monitor.PORT, prefix=monitor.SCOPE)
+except: # Old name
+    monitor.SERVER = 'statsd-exporter-svc'
+    monitor.CLIENT = statsClient(host=monitor.SERVER, port=monitor.PORT, prefix=monitor.SCOPE)
+
+
+
 #pylint: disable=too-many-instance-attributes
 class CMSRucioDatasetReplica(object):
     """
@@ -366,37 +377,38 @@ def dataset_replica_update(dataset, pnn, rse, pcli, account, dry):
 
 @timer
 def _replica_update(dataset, pnn, rse, pcli, rcli, dry):
-    ret = CMSRucioDatasetReplica(
-        rds=dataset,
-        pnn=pnn,
-        rse=rse,
-        pcli=pcli,
-        rcli=rcli
-    ).update(
-        dry=dry
-    )
+    with monitor.record_timer_block('cms_sync.update_replica'):
+        ret = CMSRucioDatasetReplica(
+            rds=dataset,
+            pnn=pnn,
+            rse=rse,
+            pcli=pcli,
+            rcli=rcli
+        ).update(
+            dry=dry
+        )
 
-    ret['replicas']['added'] = len(ret['replicas']['added'])
-    ret['replicas']['removed'] = len(ret['replicas']['removed'])
+        ret['replicas']['added'] = len(ret['replicas']['added'])
+        ret['replicas']['removed'] = len(ret['replicas']['removed'])
     return ret
 
 @timer
 def _get_dset_list(pcli, datasets):
+    with monitor.record_timer_block('cms_sync.get_dataset_list'):
+        logging.verbose("Getting datasets list for: %s",
+                        datasets)
+        ret = []
 
-    logging.verbose("Getting datasets list for: %s",
-                    datasets)
-    ret = []
+        wildcard = re.compile(r'\S*[*]\S*')
 
-    wildcard = re.compile(r'\S*[*]\S*')
+        for dset in datasets:
+            ret.extend([
+                item for
+                item in pcli.list_data_items(pditem=dset, metadata=False, locality=False)
+                if not wildcard.match(item)
+            ])
 
-    for dset in datasets:
-        ret.extend([
-            item for
-            item in pcli.list_data_items(pditem=dset, metadata=False, locality=False)
-            if not wildcard.match(item)
-        ])
-
-    ret = list(set(ret))
+        ret = list(set(ret))
 
     logging.verbose("Got %d datasets", len(ret))
 
