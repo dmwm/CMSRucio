9437ab54857f809274917edb45355029e9b8aaa4
Eric Vaandering <ericvaandering@gmail.com>
2019-09-17 13:40:04 -0500

    Drastically cleanup set_new

--- src/set_new.py
+++ src/set_new.py
@@ -1,47 +1,17 @@
 #!/usr/bin/env python
+
 from __future__ import print_function
 from rucio.client.client import Client
 
 cl = Client(account='root')
 print(cl.whoami())
 
+for campaign in ['NanoAODv5', 'Nano1June2019']:
+    filters = {'name': '*%s*' % campaign}
 
-for did in list_dids(scope='cms', filters = {'name': '*NanoAODv5*'}):
-    print(did)
-
-
-
-exit()
-
-
-
-def extract_scope(did):
-    # Try to extract the scope from the DSN
-    if did.find(':') > -1:
-        scope, name = did.split(':')[0], did.split(':')[1]
-        if name.endswith('/'):
-            name = name[:-1]
-        return scope, name
-    else:
-        scope = did.split('.')[0]
-        if did.startswith('user') or did.startswith('group'):
-            scope = ".".join(did.split('.')[0:2])
-        if did.endswith('/'):
-            did = did[:-1]
-        return scope, did
-
-
-
-
-
-with open('nanoaodv4.txt') as datasets:
-    for did in datasets:
-        if not did:
-            continue
-        print('Fixing up %s' % did)
-        did = did.strip()
-        scope, name = extract_scope(did)
+    for did in cl.list_dids(scope='cms', filters=filters):
         try:
-            cl.set_metadata(scope, name, 'is_new', True)
+            print('Fixing up %s' % did)
+            cl.set_metadata(scope='cms', name=did, key='is_new', value=True)
         except:
             print(' failed')
