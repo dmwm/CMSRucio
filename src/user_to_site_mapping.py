63d4dcdd0724ff2bf7967095b65bb78a79688364
Fabio Condomitti <facondom@lxplus706.cern.ch>
2019-09-26 01:23:53 +0200

    Refactor into OO design and added comments

--- src/user_to_site_mapping.py
+++ src/user_to_site_mapping.py
@@ -1,230 +1,144 @@
 #! /usr/bin/env python
 
-from rucio.client import Client
-from rucio.common.exception import RSENotFound, AccountNotFound, AccessDenied, RSEOperationNotSupported
-
-import os
+import getopt
 import sys
 import json
 import urllib2
 
-import utility as util
+from rucio.client import Client
+from rucio.common.exception import AccountNotFound
 
-# rucio Client
+from institute_policy import InstitutePolicy
+sys.path.insert(1, './tests')
+from policy_test import TestPolicy
+from cric_user import CricUser
+
+
 client = Client()
+institute_policy = InstitutePolicy()
+test_policy = TestPolicy()
+cric_user_list = []
 
-# dictionary with US-based only CRIC users, their RSEs and the corresponding quotas
-cric_user = {}
+"""
+This function loads the JSON file by CRIC API or by local file depending on the dry_run option.
+"""
 
+def load_cric_users(policy, dry_run):
+    if not dry_run:
+        worldwide_cric_users = json.load(urllib2.urlopen(policy.CRIC_USERS_API))
+    else:
+        sys.stdout.write('\t- dry_run version with the new fake user loaded\n')
+        with open('fake_cric_users.json') as json_file:
+            worldwide_cric_users = json.load(json_file)
+    return worldwide_cric_users
 
-def mapping_by_country(institute, country):
-    """
-    This function returns a site based on the user's institute and institute country.
-    If institute = '' the user no longer belong to CMS Experiment.
-    """
-    if not institute:
-        return
+"""
+For each CRIC user build a CricUser object with all the info needed to apply the US CMS policy in Rucio.
+"""
 
-    institutes_by_country = util.rses_by_country[country]
+def map_cric_users(country, option, dry_run):
+    worldwide_cric_users = load_cric_users(institute_policy, dry_run)
 
-    for rse_key, institutes_val in institutes_by_country.items():
-        if institute in institutes_val:
-            return rse_key + "_Test"
+    for key, user in worldwide_cric_users.items():            
+        if option == 'delete-all':
+            try:
+                username = user['profiles'][0]['login'].encode("utf-8")
+            except Exception,KeyError:
+                continue
+            for rse, val in client.get_account_limits(username).items(): 
+                client.delete_account_limit(username, rse)
 
-
-def user_to_site_mapping_by_country(country):
-    """
-    This function gets all the CRIC user profiles in JSON format by using
-    CRIC API. For each user extracts relevant information to run the algorithm.
-    Then update the cric_user dictionary.
-    """
-    cric_global_user = json.load(urllib2.urlopen(util.CRIC_URL))
-
-    for key, user in cric_global_user.items():
         institute_country = user['institute_country'].encode("utf-8")
-
-        if country in institute_country:
-            # name = key.encode("utf-8")
-            dn = user['dn'].encode("utf-8")
-            institute = user['institute'].encode("utf-8")
-            email = user['email'].encode("utf-8")
-            username = user['profiles'][0]['login'].encode("utf-8")
-            if username not in cric_user:
-                tmp = create_dict_entry(username, dn, email, institute, institute_country)
-                if not tmp:
-                    continue
-
-                cric_user.update(tmp)
-
-
-# return a dictionary entry
-def create_dict_entry(username, dn, email, institute, institute_country):
-    """
-    This utility function obtains the right RSE for the current user based on its institute
-    and build and return a dictionary entry with the correct user to site mapping.
-    Assigns a default quota at that site.
-    """
-    rse_key = mapping_by_country(institute, institute_country)
-
-    if not rse_key:  # user no longer in CMS
-        return
-
-    client.set_account_limit(account=username, rse=rse_key, bytes=util.DEFAULT_RSE_SIZE)
-
-    entry = {
-        username: {
-            "dn": dn,
-            "institute": institute,
-            "institute_country": institute_country,
-            "email": email,
-            "quota": {
-                rse_key: util.DEFAULT_RSE_SIZE,
-                # 'other_rse': 0,
-
-            }
-        }
-    }
-    return entry
-
-
-# get user information
-def get_user(username):
-    try:
-        info = cric_user[username]
-    except KeyError:
-        raise AccountNotFound
-    return info
-
-
-def get_user_quota(username=None, rse=None, method='get'):
-    """
-    This function returns the quota for a given user at a certain site.
-    """
-    try:
-        check_user_error(username)
-    except AccountNotFound:
-        raise
-
-    try:
-        check_rse_error(username, rse, method)
-    except RSENotFound:
-        raise
-
-    limit = client.get_account_limit(account=username, rse=rse)
-    return limit[rse]
-
-
-def reset_user_quota(username=None, rse=None):
-    """
-    This function resets a given RSE for a given user to the default value.
-    """
-    try:
-        check_user_error(username)
-    except AccountNotFound:
-        raise
-
-    try:
-        check_rse_error(username=username, rse=rse, method='get')
-    except RSENotFound:
-        raise
-
-    cric_user[username]["quota"][rse] = util.DEFAULT_RSE_SIZE
-    client.set_account_limit(username, rse, util.DEFAULT_RSE_SIZE)
-
-
-def set_user_quota(username, rse, quota):
-    """
-    This function sets a new quota for a certain RSE for a given user.
-    """
-    try:
-        check_user_error(username)
-    except AccountNotFound:
-        raise
-
-    try:
-        check_rse_error(username=username, rse=rse, method='set')
-        check_quota_error(username, rse, quota, 'set')
-    except RSENotFound:
-        raise
-    except RSEOperationNotSupported:
-        raise
-
-    cric_user[username]["quota"][rse] = quota
-    client.set_account_limit(username, rse, quota)
-
-
-def check_quota_error(username=None, rse=None, quota=0, method='get'):
-    """
-    This functions checks if the new given quota is less than the current one or
-    if it has wrong values.
-    """
-    old_quota = get_user_quota(username, rse, method)
-    if old_quota is not None and (quota <= 0 or quota < int(old_quota)):
-        raise RSEOperationNotSupported
-
-
-def delete_user_quota(username=None, rse=None):
-    """
-    This function deletes the limits on an rse of a given user.
-    """
-    try:
-        check_user_error(username)
-    except AccountNotFound:
-        raise
-
-    try:
-        check_rse_error(username, rse)
-    except RSENotFound:
-        raise
-
-    del cric_user[username]["quota"][rse]
-    client.delete_account_limit(username, rse)
-
-
-def check_user_error(username=None):
-    """
-    This function checks if user is in CRIC users.
-    """
-    try:
-        user_val = cric_user[username]
-    except KeyError:
-        raise AccountNotFound
-    try:
-        client.get_account(username)
-    except AccountNotFound:
-        raise
-
-    if user_val is None:
-        raise AccountNotFound
-
-
-def check_rse_error(username=None, rse=None, method='get'):
-    """
-    This function checks for errors on the rse name.
-    Then checks if it the RSE actually exists.
-    Method parameter is used to not check if the current user has
-    quota on a given rse to reuse this function for the set method.
-    """
-    user_val = cric_user[username]
-    if rse is None:
-        raise RSENotFound
-
-    '''if rse not in user_val['quota'] and method is 'get':
-        print user_val['quota'].keys(), method
-        raise RSENotFound'''
-
-    if method is 'get':
+        institute = user['institute'].encode("utf-8")
+        dn = user['dn'].encode("utf-8")
+        email = user['email'].encode("utf-8")
+        account_type = "USER"
+        policy = ''
         try:
-            user_val['quota'][rse]
-        except KeyError:
-            raise RSENotFound
+            username = user['profiles'][0]['login'].encode("utf-8")
+            if not institute or not institute_country:
+                policy = test_policy
+                message = "TestPolicy applied to the user {0} (missing info for the US CMS policy)\n".format(username)
+                #sys.stdout.write(message)
+                raise Exception
+            elif country != "" and country in institute_country:
+                if username == 'perichmo':
+                    continue
+                policy = institute_policy
+        except Exception, KeyError:
+            continue
+
+        cric_user = CricUser(username, email, dn, account_type, institute, institute_country, policy, option)
+        cric_user_list.append(cric_user)
+        set_rucio_limits(cric_user)
+
+
+"""
+This function sets the Rucio limits, and if needed it also create a Rucio account.
+"""
+def set_rucio_limits(cric_user):
+    try:
+        client.get_account(cric_user.username)
+    except AccountNotFound:
+        client.add_account(cric_user.username, cric_user.account_type, cric_user.email)
+    for rse in cric_user.rses_list:
+        client.set_account_limit(cric_user.username, rse.sitename, rse.quota)
+
+
+def get_cric_user(username):
+    for user in cric_user_list:
+        if user.username == username:
+            return user
+    raise KeyError
+
+"""
+This function modify the policy of one user.
+"""
+def change_cric_user_policy(username, policy):
+    cric_user = get_cric_user(username)
+    cric_user.change_policy(policy)
+    set_rucio_limits(cric_user)
+
+
+def usage():
+    print "Command:\tuser_to_site_mapping.py [-o] [-d]"
+    print "Options:"
+    print "\t-h, --help"
+    print "\t-o, --option=\tset-new-only|reset-all|delete-all"
+    print "\t-d, --dry_run=\tt|f"
+
+
+def main():
+    option = 'set-new-only'
+    dry_run = False
 
     try:
-        client.get_rse(rse)['rse']
-    except RSENotFound:
-        raise
+        opts, args = getopt.getopt(sys.argv[1:], "ho:o:d:", ["help", "option=", "dry_run="])
+    except getopt.GetoptError as err:
+        # print help information and exit:
+        print str(err)  # will print something like "option -a not recognized"
+        usage()
+        sys.exit(2)
+
+    for o, a in opts:
+        if o in ("-o", "--option"):
+            option = a
+        elif o in ("-d", "--dry_run"):
+            dry_run = a
+        elif o in ("-h", "--help"):
+            usage()
+            sys.exit()
+        else:
+            usage()
+            sys.exit(2)
+
+    if option not in ['set-new-only', 'delete-all', 'reset-all']:
+        usage()
+        sys.exit(2)
+
+    map_cric_users('US', option, dry_run)
 
 
-user_to_site_mapping_by_country('US')
-
+if __name__ == '__main__':
+    main()
 
