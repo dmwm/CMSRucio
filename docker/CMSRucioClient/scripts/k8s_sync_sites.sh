f54b434f116e79b7c4927edfea28ad0e20555a75
Eric Vaandering <ericvaandering@gmail.com>
2020-05-13 10:53:43 -0500

    Remove site syncing

--- docker/CMSRucioClient/scripts/k8s_sync_sites.sh
+++ docker/CMSRucioClient/scripts/k8s_sync_sites.sh
@@ -23,22 +23,22 @@ echo "Syncing account roles for managers and group accounts"
 ./syncRolesToAttributes.py
 ./syncRolesToGroupAccounts.py
 
-echo "Syncing RSEs"
-./cmsrses.py --pnn all --select 'T2_\S+' --exclude 'T2_MY_\S+' --exclude '\S+CERN\S+' --type real --type temp --type test --fts https://fts3.cern.ch:8446
-./cmsrses.py --pnn all --select 'T1_\S+' --exclude '.*MSS' --exclude '\S+_Tape_Test' --type real --type test --fts https://fts3.cern.ch:8446
-./cmsrses.py --pnn all --select 'T3_US_NotreDame' --select 'T3_CH_CERN_OpenData' --select  'T3_US_Rutgers' \
-                       --select 'T3_US_UMiss' --select 'T3_FR_IPNL' --select 'T3_US_MIT' --select 'T2_US_OSU' --select 'T3_KR_KISTI' \
-                       --select 'T3_US_Colorado' --select 'T3_US_FNALLPC' --select 'T3_US_Baylor' --select 'T3_IT_Trieste' \
-                       --select 'T3_US_UMD' --select 'T3_US_Princeton_ICSE' --type real --fts https://fts3.cern.ch:8446
+#echo "Syncing RSEs"
+#./cmsrses.py --pnn all --select 'T2_\S+' --exclude 'T2_MY_\S+' --exclude '\S+CERN\S+' --type real --type temp --type test --fts https://fts3.cern.ch:8446
+#./cmsrses.py --pnn all --select 'T1_\S+' --exclude '.*MSS' --exclude '\S+_Tape_Test' --type real --type test --fts https://fts3.cern.ch:8446
+#./cmsrses.py --pnn all --select 'T3_US_NotreDame' --select 'T3_CH_CERN_OpenData' --select  'T3_US_Rutgers' \
+#                       --select 'T3_US_UMiss' --select 'T3_FR_IPNL' --select 'T3_US_MIT' --select 'T2_US_OSU' --select 'T3_KR_KISTI' \
+#                       --select 'T3_US_Colorado' --select 'T3_US_FNALLPC' --select 'T3_US_Baylor' --select 'T3_IT_Trieste' \
+#                       --select 'T3_US_UMD' --select 'T3_US_Princeton_ICSE' --type real --fts https://fts3.cern.ch:8446
+
 # OR ./setRucioFromGitlab with some new parameters
 
-echo "Creating sync accounts"
-./syncaccounts.py --identity "/DC=ch/DC=cern/OU=Organic Units/OU=Users/CN=cmsrucio/CN=430796/CN=Robot: CMS Rucio Data Transfer" --type x509
+#echo "Creating sync accounts"
+#./syncaccounts.py --identity "/DC=ch/DC=cern/OU=Organic Units/OU=Users/CN=cmsrucio/CN=430796/CN=Robot: CMS Rucio Data Transfer" --type x509
 
 echo "Syncing quotas"
 ./setSiteQuotas
 
-echo "Creating links"
-# Remove the --disable flag for Katy's RSE
-./cmslinks.py --phedex_link --overwrite
-# ./cmslinks.py --phedex_link --overwrite --disable
+#echo "Creating links"
+#./cmslinks.py --phedex_link --overwrite # Remove the --disable flag for Katy's RSE
+## ./cmslinks.py --phedex_link --overwrite --disable
