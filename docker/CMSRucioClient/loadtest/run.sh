6497971f6b42b16c6e83b717d7f6193a8298e15b
Nick Smith <nick.smith@cern.ch>
2020-11-06 18:24:40 -0600

    Loadtest implementation
    Make loadtest more daemon-like and implement poisson timing
    
    Use exponential falloff after minimum delay time
    
    Refine timing
    
    Deal with some pesky T2s
    
    float division
    
    Judge repairer actually tries immediately
    
    So the lag compensation is not correct
    
    Some more tweaks for full T12 matrix
    
    Move loadtest

---
+++ docker/CMSRucioClient/loadtest/run.sh
@@ -0,0 +1,10 @@
+# voms-proxy-init --voms cms:/cms/Role=production --cert ruciocert.pem --key ruciokey.pem
+docker run -it --rm \
+  -v $(voms-proxy-info --path):/tmp/x509up \
+  -v $PWD/rucio.cfg:/opt/rucio/etc/rucio.cfg \
+  -v $PWD/loadtest.py:/root/loadtest.py \
+  -e RUCIO_ACCOUNT=transfer_ops \
+  --entrypoint /usr/bin/python \
+  cmssw/rucio_client loadtest.py -v \
+  --source_rse_expression "rse_type=disk&cms_type=real&tier<3" \
+  --dest_rse_expression "rse_type=disk&cms_type=real&tier<3"
