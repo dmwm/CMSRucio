85677bda536f01ce461b7646727b42c91d270c93
Nick Smith <nick.smith@cern.ch>
2020-11-17 18:16:09 -0600

    Make a docker container

--- docker/CMSRucioClient/loadtest/run.sh
+++ docker/CMSRucioClient/loadtest/run.sh
@@ -1,10 +1,13 @@
-# voms-proxy-init --voms cms:/cms/Role=production --cert ruciocert.pem --key ruciokey.pem
+#!/bin/bash
+
+# To run loadtest script in docker container for testing, you first need a production proxy:
+# $ voms-proxy-init --voms cms:/cms/Role=production --cert ruciocert.pem --key ruciokey.pem
+# The loadtest_client container is built in the parent directory with:
+# $ docker build -t loadtest_client -f Dockerfile.loadtest .
+# The loadtest.py executable is the entrypoint
+
 docker run -it --rm \
   -v $(voms-proxy-info --path):/tmp/x509up \
-  -v $PWD/rucio.cfg:/opt/rucio/etc/rucio.cfg \
-  -v $PWD/loadtest.py:/root/loadtest.py \
-  -e RUCIO_ACCOUNT=transfer_ops \
-  --entrypoint /usr/bin/python \
-  cmssw/rucio_client loadtest.py -v \
-  --source_rse_expression "rse_type=disk&cms_type=real&tier<3" \
-  --dest_rse_expression "rse_type=disk&cms_type=real&tier<3"
+  -e RUCIO_HOME=/opt/rucio-prod/ \
+  loadtest_client \
+  -v --source_rse_expression "T2_US_MIT_Test" --dest_rse_expression "T2_US_UCSD_Test"
