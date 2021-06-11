ba75ed500dd557727309d542b06b1b4f98ce7820
Eric Vaandering <ericvaandering@gmail.com>
2021-06-11 16:27:35 +0200

    Updated stuff for Int server

---
+++ src/IntLinks.py
@@ -0,0 +1,252 @@
+#! /bin/env python3
+"""
+Class definition for the distances (links) among CMS RSEs.
+And script for updating the distances.
+"""
+
+import argparse
+import json
+import logging
+import os
+import re
+
+from rucio.client import Client
+
+DEFAULT_EXCLUDE_LINKS = (
+    {'dest': {'type': 'temp'}, 'src': {}},
+)
+
+GLOBUS_RSES = ['T3_US_Theta']
+
+DEFAULT_DISTANCE_RULES = {'site': 1, 'region&country': 4, 'country': 7, 'region': 10, 'other': 13}
+
+
+class LinksMatrix(object):
+    """
+    CMS RSE distances according to a set of rules
+    """
+
+    def __init__(self, account, auth_type=None, exclude=DEFAULT_EXCLUDE_LINKS,
+                 distance=None, rselist=None):
+
+        if distance is None:
+            distance = DEFAULT_DISTANCE_RULES
+
+        self.rcli = Client(account=account, auth_type=auth_type)
+
+        self._get_rselist(rselist)
+
+        self._get_matrix(distance, exclude)
+
+    def _get_rselist(self, rselist=None):
+
+        self.rselist = []
+
+        if rselist is None:
+            rselist = [rse['rse'] for rse in self.rcli.list_rses()]
+
+        for rse in rselist:
+            attrs = self.rcli.list_rse_attributes(rse=rse)
+
+            try:
+                self.rselist.append({
+                    'rse': rse,
+                    'pnn': attrs['pnn'],
+                    'type': attrs['cms_type'],
+                    'country': attrs['country'],
+                    'region': attrs.get('region', None)
+                })
+            except KeyError:
+                logging.warning('No expected attributes for RSE %s. Skipping',
+                                rse)
+
+    def _get_matrix(self, distance, exclude):
+
+        matrix = {}
+
+        self.links = {}
+
+        for src in self.rselist:
+            for dest in self.rselist:
+
+                src_rse = src['rse']
+                dest_rse = dest['rse']
+                src_pnn = src['pnn']
+                dest_pnn = dest['pnn']
+
+                if dest_pnn == src_pnn:
+                    link = distance['site']
+                elif src['region'] and dest['region'] and src['region'] == dest['region']:
+                    if src['country'] == dest['country']:
+                        link = distance['region&country']
+                    else:
+                        link = distance['region']
+                elif src_pnn in matrix and dest_pnn in matrix[src_pnn]:
+                    link = distance['site'] - matrix[src_pnn][dest_pnn]
+                else:
+                    if src['country'] == dest['country']:
+                        link = distance['country']
+                    else:
+                        link = distance['other']
+
+                if src_rse not in self.links:
+                    self.links[src_rse] = {}
+
+                self.links[src_rse][dest_rse] = link
+
+        self._filter_matrix(exclude)
+
+    def _filter_matrix(self, exclude):
+
+        for src in self.rselist:
+            for dest in self.rselist:
+
+                if src['rse'] == dest['rse']:
+                    continue
+
+                for rule in exclude:
+                    matched = True
+
+                    for item in rule['src']:
+                        if not re.match(rule['src'][item], src[item]):
+                            matched = False
+
+                    for item in rule['dest']:
+                        if not re.match(rule['dest'][item], dest[item]):
+                            matched = False
+
+                    if matched:
+                        self.links[src['rse']][dest['rse']] = -1
+                        break
+
+    def update(self, overwrite=False, disable=True, dry=False, srcselect=r'\S+', dstselect=r'\S+'):
+        """
+        Updates distances according to what is expected
+        :overwrite:   overwrite distance of the links that already exist
+        :disable:     set ranking to 0 for the links that should be disabled
+        :dry:         dry run
+        """
+
+        count = {'checked': [], 'created': [], 'updated': [], 'disabled': []}
+
+        src_regex = re.compile(srcselect)
+        dst_regex = re.compile(dstselect)
+
+        for src in self.rselist:
+            srse = src['rse']
+            logging.info("Setting links from %s to %s other RSEs.", srse, len(self.rselist))
+            for dest in self.rselist:
+                drse = dest['rse']
+
+                if srse == drse or not src_regex.match(srse) or not dst_regex.match(drse):
+                    continue
+
+                if ((srse in GLOBUS_RSES and drse not in GLOBUS_RSES) or (drse in GLOBUS_RSES and srse not in GLOBUS_RSES)):
+                    logging.info("Not setting link from %s to %s", srse, drse)
+                    continue
+                    
+                count['checked'].append([srse, drse])
+
+                # Todo.. doublecheck I'm not reversing things
+                link = self.rcli.get_distance(srse, drse)
+
+                if srse in self.links and drse in self.links[srse] and self.links[srse][drse] >= 0:
+                    if not link:
+                        pars = {'distance': 1, 'ranking': self.links[srse][drse]}
+
+                        if dry:
+                            logging.info("adding link from %s to %s with %s. Dry Run", srse, drse, str(pars))
+                        else:
+                            self.rcli.add_distance(srse, drse, pars)
+
+                        count['created'].append([srse, drse])
+
+                    elif link and overwrite:
+                        if dry:
+                            logging.info("setting distance %s for link from %s to %s. Dry run.",
+                                         self.links[srse][drse], srse, drse)
+                        else:
+                            self.rcli.update_distance(srse, drse,
+                                                      {'ranking': self.links[srse][drse],
+                                                       'distance': 1
+                                                       })
+
+                        count['updated'].append([srse, drse])
+
+                elif link and disable:
+                    if dry:
+                        logging.info("disabling link from %s to %s. Dry run", srse, drse)
+                    else:
+                        self.rcli.update_distance(srse, drse, {
+                            'ranking': None,
+                            'distance': None,
+                        })
+
+                    count['disabled'].append([srse, drse])
+
+        return count
+
+
+if __name__ == '__main__':
+    PARSER = argparse.ArgumentParser(
+        description='''CLI for updating a CMS RSE's links.''',
+    )
+    PARSER.add_argument('-v', '--verbose', dest='debug', action='store_true',
+                        help='increase output verbosity')
+    PARSER.add_argument('-t', '--dry', dest='dry', action='store_true',
+                        help='only printout what would have been done')
+    PARSER.add_argument('--rse', dest='rselist', help='RSE. Can be multiple, default all.',
+                        action='append', default=None)
+    PARSER.add_argument('--srcselect', dest='srcselect',
+                        help='Regex for selecting the source RSEs.',
+                        default=r'\S+')
+    PARSER.add_argument('--dstselect', dest='dstselect',
+                        help='Regex for selecting the destination RSEs.',
+                        default=r'\S+')
+    PARSER.add_argument('--distance', dest='distance', default=None,
+                        help='rules for different RSE distances, default %s' %
+                             json.dumps(DEFAULT_DISTANCE_RULES))
+    PARSER.add_argument('--exclude', dest='exclude', default=None,
+                        help='exclde rules for links, default %s' %
+                             json.dumps(DEFAULT_EXCLUDE_LINKS))
+    PARSER.add_argument('--account', dest='account',
+                        default=os.environ['RUCIO_ACCOUNT'],
+                        help='Rucio account. default RUCIO_ACCOUNT')
+    PARSER.add_argument('--overwrite', dest='overwrite', action='store_true',
+                        help='Overwrite distances that have changed.')
+    PARSER.add_argument('--disable', dest='disable', action='store_true',
+                        help='Disable links that should not be there.')
+
+    OPTIONS = PARSER.parse_args()
+
+    if OPTIONS.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    if OPTIONS.exclude:
+        OPTIONS.exclude = json.loads(OPTIONS.exclude.replace("'", '"'))
+    else:
+        OPTIONS.exclude = DEFAULT_EXCLUDE_LINKS
+
+    COUNT = LinksMatrix(
+        account=OPTIONS.account,
+        exclude=OPTIONS.exclude,
+        distance=OPTIONS.distance,
+        rselist=OPTIONS.rselist,
+    ).update(
+        overwrite=OPTIONS.overwrite,
+        disable=OPTIONS.disable,
+        dry=OPTIONS.dry,
+        srcselect=OPTIONS.srcselect,
+        dstselect=OPTIONS.dstselect
+    )
+
+    logging.debug(str(COUNT['checked']))
+    logging.debug(str(COUNT['updated']))
+    logging.debug(str(COUNT['disabled']))
+    logging.debug(str(COUNT['created']))
+
+    logging.info("Link summary: updated %d, disabled %d, created %d;",
+                 len(COUNT['updated']), len(COUNT['disabled']), len(COUNT['created']))
+
--- src/IntSetupRSEs.py
+++ src/IntSetupRSEs.py
@@ -8,10 +8,19 @@ import re
 from rucio.client import Client
 from rucio.common.exception import RSEProtocolNotSupported, RSENotFound, Duplicate
 
-RSES_TO_SET = ['T1_US_FNAL_Disk', 'T2_CH_CERN', 'T2_US_Purdue']
-RSES_WITHOUT_INPUT = [] # ['T0_CH_CERN_Disk']
+RSES_TO_SET = ['T1_US_FNAL_Disk', 'T2_CH_CERN', 'T2_US_Purdue',]
+RSES_WITHOUT_INPUT = ['T3_US_NERSC', 'T2_US_Wisconsin', 'T2_US_Nebraska', 'T2_US_UCSD'] 
+RSES_ONLY_INPUT = ['T1_US_FNAL_Tape', 'T0_CH_CERN_Tape', 'T1_IT_CNAF_Tape']
+
+#RSES_TO_SET = ['T1_US_FNAL_Disk']
+#RSES_WITHOUT_INPUT = []
+#RSES_ONLY_INPUT = []
+
+
+
+
 INT_SETTINGS = {'availability_read': True, 'availability_write': True, 'availability_delete': True}
-INT_ATTRIBUTES = {'ddm_quota': 0, 'cms_type': 'int', 'reaper': False}
+INT_ATTRIBUTES = {'ddm_quota': 0, 'cms_type': 'int', 'reaper': True}
 INPUT_SETTINGS = {'availability_read': True, 'availability_write': False, 'availability_delete': False}
 INPUT_ATTRIBUTES = {'ddm_quota': 0, 'cms_type': 'input', 'reaper': False}
 
@@ -63,8 +72,11 @@ def overwrite_protocols(client, rse_name, new_protocols=None):
 
     for new_proto in new_protocols:
         if new_proto['scheme'] in ['srm', 'srmv2', 'gsiftp', 'root', 'davs']:
-            print('Adding %s to %s' % (new_proto['scheme'], rse_name))
-            client.add_protocol(rse=rse_name, params=new_proto)
+            try: 
+                print('Adding %s to %s' % (new_proto['scheme'], rse_name))
+                client.add_protocol(rse=rse_name, params=new_proto)
+            except Duplicate:
+                print('Could not add additional %s to %s' % (new_proto['scheme'], rse_name))
 
 
 def set_rse_attributes(client, rse_name, attributes):
@@ -93,17 +105,23 @@ def rewrite_protocols(protocols, pfns, read_write=False, enforce_prefix=True):
 
     :return: The dictionary of rewritten protocols
     """
+#    import pdb; pdb.set_trace()
     new_protocols = []
     scheme, hostname, port, prefix = None, None, None, None  # Supress warnings
     if enforce_prefix:
         regexes = [r'(?P<scheme>\w+)://(?P<host>[a-zA-Z0-9\-\.]+):(?P<port>\d+)/(?P<prefix>.*)',
-                   r'(?P<scheme>\w+)://(?P<host>[a-zA-Z0-9\-\.]+)/(?P<prefix>.*)']
+                   r'(?P<scheme>\w+)://(?P<host>[a-zA-Z0-9\-\.]+)/(?P<prefix>.*)',
+                   r'(?P<scheme>\w+)://(?P<host>[a-zA-Z0-9\-\.]+):(?P<port>\d+)(?P<wsp>\/.*\=)(?P<prefix>.*)',
+                   r'(?P<scheme>\w+)://(?P<host>[a-zA-Z0-9\-\.]+)(?P<wsp>\/.*\=)(?P<prefix>.*)',
+ ]
+#{'type': 1, 'regexp': re.compile(r'(\w+)://([a-zA-Z0-9\-\.]+):(\d+)(\/.*\=)(.*)')},
+
         int_root = pfns['cms:/store/test/rucio/int/']
         for regex in regexes:
             try:
                 prefix_regex = re.compile(regex)
                 prefix_match = prefix_regex.match(int_root)
-
+                print('Found a match for %s ' % regex)
                 scheme = prefix_match.group('scheme')
                 hostname = prefix_match.group('host')
                 try:
@@ -120,7 +138,7 @@ def rewrite_protocols(protocols, pfns, read_write=False, enforce_prefix=True):
                        'wan': {'delete': 1, 'read': 1, 'third_party_copy': 1, 'write': 1}}
     else:
         new_domains = {'lan': {'delete': 0, 'read': 0, 'write': 0},
-                       'wan': {'delete': 0, 'read': 1, 'third_party_copy': 0, 'write': 0}}
+                       'wan': {'delete': 0, 'read': 1, 'third_party_copy': 1, 'write': 0}}
 
     for protocol in protocols:
         new_protocol = copy.deepcopy(protocol)
@@ -131,13 +149,14 @@ def rewrite_protocols(protocols, pfns, read_write=False, enforce_prefix=True):
             new_protocol['extended_attributes'] = None
             new_protocol['prefix'] = prefix
             # Need to handle SRM separately
-            if 'web_service_path' in protocol['extended_attributes']:
-                web_path = protocol['extended_attributes']['web_service_path']
+            extended_attributes = protocol['extended_attributes'] or {}
+            if 'web_service_path' in extended_attributes:
+                web_path = extended_attributes['web_service_path']
                 new_protocol['extended_attributes'] = {}
                 new_protocol['extended_attributes']['web_service_path'] = web_path
                 prefix = prefix.replace(web_path, '')
                 new_protocol['prefix'] = prefix
-        if protocol['scheme'] in ['gsiftp']:
+        if protocol['scheme'] in ['gsiftp', 'srm']:
             new_protocols.append(new_protocol)
 
     return new_protocols
@@ -149,24 +168,29 @@ if __name__ == '__main__':
     rcp = Client(rucio_host='http://cms-rucio.cern.ch', auth_host='https://cms-rucio-auth.cern.ch',
                  account='ewv')
 
-    for rse_name in set(RSES_TO_SET+RSES_WITHOUT_INPUT):
+    input_rses = set(RSES_TO_SET+RSES_ONLY_INPUT)
+    write_rses = set(RSES_TO_SET+RSES_WITHOUT_INPUT)
+
+    for rse_name in set(RSES_TO_SET+RSES_WITHOUT_INPUT+RSES_ONLY_INPUT):
         # Fetch the values needed from the production RSE
         pfns = rcp.lfns2pfns(rse_name, ['cms:/store/test/rucio/int/'], operation='read')
         rse = rcp.get_rse(rse_name)
         rse_attributes = rcp.list_rse_attributes(rse=rse_name)
 
         # Setup the integration RSE
-        int_rse = rse_name 
-        settings = INT_SETTINGS
-        int_attributes = copy.deepcopy(rse_attributes)
-        del int_attributes[rse_name]
-        int_attributes.update(INT_ATTRIBUTES)
-        create_rse(client=rci, rse_name=int_rse, settings=settings)
-        set_rse_attributes(client=rci, rse_name=int_rse, attributes=int_attributes)
-        rse_protocols = rse['protocols']
-        int_protocols = rewrite_protocols(rse_protocols, pfns, read_write=True)
-        overwrite_protocols(client=rci, rse_name=int_rse, new_protocols=int_protocols)
-        if rse_name in RSES_TO_SET:
+        if rse_name in write_rses:
+            int_rse = rse_name 
+            settings = INT_SETTINGS
+            int_attributes = copy.deepcopy(rse_attributes)
+            del int_attributes[rse_name]
+            int_attributes.update(INT_ATTRIBUTES)
+            create_rse(client=rci, rse_name=int_rse, settings=settings)
+            set_rse_attributes(client=rci, rse_name=int_rse, attributes=int_attributes)
+            rse_protocols = rse['protocols']
+            #import pdb; pdb.set_trace()
+            int_protocols = rewrite_protocols(rse_protocols, pfns, read_write=True)
+            overwrite_protocols(client=rci, rse_name=int_rse, new_protocols=int_protocols)
+        if rse_name in input_rses:
             input_rse = rse_name + '_Input'
             settings = INPUT_SETTINGS
             input_attributes = copy.deepcopy(rse_attributes)
