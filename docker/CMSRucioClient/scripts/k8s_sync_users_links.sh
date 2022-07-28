c41229e1a733077886ac82d63150734d4744d46f
dciangot <dciangot@cern.ch>
2022-07-28 06:17:24 +0000

    merge oidc sync script with the x509 one

--- docker/CMSRucioClient/scripts/k8s_sync_users_links.sh
+++ docker/CMSRucioClient/scripts/k8s_sync_users_links.sh
@@ -33,3 +33,20 @@ fi
 
 echo "Creating links"
 ./cmslinks.py --overwrite --disable
+
+# Preparing oidc-agent to get/refresh token to retrieve OIDC user information
+export OIDC_CONFIG_DIR=$HOME/.oidc-agent
+
+eval $(oidc-keychain)
+
+oidc-gen cms --issuer "https://$IAM_SERVER/" \
+    --client-id "$IAM_CLIENT_ID" \
+    --client-secret "$IAM_CLIENT_SECRET" \
+    --rt "$REFRESH_TOKEN" \
+    --confirm-yes \
+    --scope "scim:read" \
+    --redirect-uri http://localhost:8843 \
+    --pw-cmd "echo \"DUMMY PWD\""
+
+echo "Syncing OIDC user identities"
+./syncaccount_oidc.py
