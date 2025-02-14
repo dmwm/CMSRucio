8f2a25d3990339e7045a6541e5e37d3a58064d6d
voetberg <magpie127@gmail.com>
2025-02-14 08:14:33 -0600

    Tape collocation policy plugin

--- src/policy/CMSRucioPolicy/algorithms/tape_collocation.py
+++ src/policy/CMSRucioPolicy/algorithms/tape_collocation.py
@@ -1,10 +1,7 @@
 '''
 
 '''
-from configparser import DuplicateSectionError
-
 from rucio.transfertool.fts3_plugins import FTS3TapeMetadataPlugin
-from rucio.common.config import config_set, config_add_section, config_get_list
 from rucio.core.did import list_parent_dids
 from rucio.db.sqla.session import get_session 
 
@@ -17,16 +14,6 @@ class CMSTapeCollocation(FTS3TapeMetadataPlugin):
             func= lambda x: self._collocation(self.cms_collocation, x), 
             init_func=self.instance_init
         )
-        # Add the plugin to the config
-        try: 
-            config_add_section("transfers")
-        except DuplicateSectionError: 
-            pass 
-
-        active_plugins = config_get_list("transfers", "fts3tape_metadata_plugins", raise_exception=False, default=[])
-        if "tape_collocation" not in active_plugins: 
-            active_plugins.append("tape_collocation")
-            config_set("transfers", "fts3tape_metadata_plugins", active_plugins)
     
     def instance_init(self): 
         self.session = get_session()
@@ -35,13 +22,16 @@ class CMSTapeCollocation(FTS3TapeMetadataPlugin):
         self.allowed_types = ['data', 'hidata', 'mc', 'himc', 'relval', 'hirelval']
 
     def parent_container(self, scope, name): 
-        parent_container = [
+        containers = [
             f"{parent['scope']}:{parent['name']}"
             for parent 
             in list_parent_dids(scope=scope, name=name, session=self.session) 
             if parent['type']=='CONTAINER'
-        ][0]
-        return parent_container
+        ]
+        try: 
+            return containers[0]
+        except IndexError: 
+            pass
 
     def data_type(self, name): 
         data_type = name.lstrip('/store/').split('/')[0] # First index that isn't `store`
@@ -50,12 +40,19 @@ class CMSTapeCollocation(FTS3TapeMetadataPlugin):
         return data_type
 
     def data_tier(self, data_type, name): 
-        if data_type in self.allowed_types: 
-            return name.split('/')[4]
+        try: 
+            if data_type in self.allowed_types: 
+                return name.split('/')[4]
+        except IndexError: 
+            pass  # Can't get the tier
 
     def era(self, data_type, name): 
-        if data_type in self.allowed_types: 
-            return name.lstrip('/store/').split('/')[1]
+        try: 
+            if data_type in self.allowed_types: 
+                return name.lstrip('/store/').split('/')[1]
+        except IndexError: 
+            pass  # Can't get the era
+
 
     def cms_collocation(self, *hints):
         """
@@ -69,27 +66,32 @@ class CMSTapeCollocation(FTS3TapeMetadataPlugin):
         Data tier - either in the LFN or the end of the parent container
 
         Level 2
-        Era (which I think for MC is the Campaign)
+        Era (which for MC is the Campaign)
 
         Level 3 
         Parent Container (can either get this explicitly or get the parent dataset and lop off the hash mark and the hash following it)
         """
 
+        # Notes - Add it to the cms-rucio-common yaml in dmwcore/rucio-flux
+        # Levels common < daemons < prod-deamons 
+        # put in PR 
+
         lfn = hints['name']
         data_type = self.data_type(lfn)
         tier = self.data_tier(data_type, lfn)
         era = self.era(data_type, lfn)
+        parent = self.parent_container(hints['scope'], hints['name'])
 
         collocation = {
             "0": data_type,
-            "3": self.parent_container(hints['scope'], hints['name'])
         }
 
         if tier is not None: 
             collocation['1'] = tier 
         if era is not None: 
             collocation['2'] = era
-
+        if parent is not None: 
+            collocation['3'] = parent
         return collocation
     
-CMSTapeCollocation(algorithm="def") # Registering the plugin and adding it to the config 
+CMSTapeCollocation(algorithm="def") # Registering the plugin
