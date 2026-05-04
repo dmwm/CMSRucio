91457420287e665b0521dcf95376d73ee56fddf2
Christos Emmanouil <chris19973@gmail.com>
2026-05-04 14:25:14 +0200

    file-integrity-check: integration to the file invalidation server: add managed = False to both models

--- DMOps/file_invalidation_server/file_integrity_checker/models.py
+++ DMOps/file_invalidation_server/file_integrity_checker/models.py
@@ -20,6 +20,7 @@ class FileIntegrityRequest(models.Model):
     updated_at     = models.DateTimeField(auto_now=True)
 
     class Meta:
+        managed  = False
         db_table = 'file_integrity_checker_requests'
         ordering = ['-created_at']
 
@@ -41,6 +42,7 @@ class FileReplica(models.Model):
     status = models.CharField(max_length=20)
 
     class Meta:
+        managed  = False
         db_table = 'file_integrity_checker_replicas'
 
     def __str__(self):
